f953c4da1a7df7afbc15894b7b0cd25a485b84f7
rsx-software <software@rsx.skule.ca>
2026-05-22 02:43:04 -0400

    .

--- src/falcon_motor_control.cpp
+++ src/falcon_motor_control.cpp
@@ -16,11 +16,13 @@ class FalconMotorController : public rclcpp::Node {
 public:
   FalconMotorController() : rclcpp::Node("falcon_motor_controller") {
     // Params (tunable)
-    radius_            = this->declare_parameter<double>("radius", 0.8);
+    radius_inner_         = this->declare_parameter<double>("radius_inner", 0.8);
+    radius_outer_         = this->declare_parameter<double>("radius_outer", 1.0);
     max_linear_speed_  = this->declare_parameter<double>("max_linear_speed", 2.5);
-    max_delta_         = this->declare_parameter<double>("max_delta", 0.02);
+    max_delta_         = this->declare_parameter<double>("max_delta", 0.015);
 
-    max_angular_speed_ = max_linear_speed_ * radius_;
+    max_inner_angular_speed_ = max_linear_speed_ * radius_inner_;
+    max_outer_angular_speed_ = max_linear_speed_ * radius_outer_;
 
     // Subs
     cmd_sub_ = this->create_subscription<geometry_msgs::msg::Twist>(
@@ -56,63 +58,88 @@ private:
 
   void cmdCallback(const geometry_msgs::msg::Twist::SharedPtr msg) {
     double move = msg->linear.x / max_linear_speed_;
-    double rotate = radius_ * (msg->angular.z / max_angular_speed_);
+    double rotate_inner = radius_inner_ * (msg->angular.z / max_inner_angular_speed_);
+    double rotate_outer = radius_outer_ * (msg->angular.z / max_outer_angular_speed_);
 
     // Normalize to [-1, 1]
-    double s = std::max(std::abs(move), std::abs(rotate));
+    double s = std::max(std::abs(move), std::max(std::abs(rotate_inner), std::abs(rotate_outer)));
     if (s > 1.0) {
       move   /= s;
-      rotate /= s;
+      rotate_inner /= s;
+      rotate_outer /= s;
     }
 
     if (move > 0.0) {
-      if (rotate > 0.0) {
-        left_out_  = move - rotate;
-        right_out_ = std::max(move, rotate);
+      if (rotate_inner > 0.0) {
+        bl_out_  = move - rotate_inner;
+        br_out_ = std::max(move, rotate_inner);
+        fl_out_  = move - rotate_outer;
+        fr_out_ = std::max(move, rotate_outer);
       } else {
-        left_out_  = std::max(move, -rotate);
-        right_out_ = move + rotate;
+        bl_out_  = std::max(move, -rotate_inner);
+        br_out_ = move + rotate_inner;
+        fl_out_  = std::max(move, -rotate_outer);
+        fr_out_ = move + rotate_outer;
       }
     } else {
-      if (rotate > 0.0) {
-        left_out_  = -std::max(-move, rotate);
-        right_out_ = move + rotate;
+      if (rotate_outer > 0.0) {
+        bl_out_  = -std::max(-move, rotate_inner);
+        br_out_ = move + rotate_inner;
+        fl_out_  = -std::max(-move, rotate_outer);
+        fr_out_ = move + rotate_outer;
       } else {
-        left_out_  = move - rotate;
-        right_out_ = -std::max(-move, -rotate);
+        bl_out_  = move - rotate_inner;
+        br_out_ = -std::max(-move, -rotate_inner);
+        fl_out_  = move - rotate_outer;
+        fr_out_ = -std::max(-move, -rotate_outer);
       }
     }
   }
 
   void publishLoop() {
     // Rate limit changes
-    double left_cmd  = clampDelta(last_left_out_,  left_out_,  max_delta_);
-    double right_cmd = clampDelta(last_right_out_, right_out_, max_delta_);
+    double fl_cmd = clampDelta(last_fl_out_,  fl_out_,  max_delta_);
+    double fr_cmd = clampDelta(last_fr_out_, fr_out_, max_delta_);
+    double bl_cmd = clampDelta(last_bl_out_, bl_out_, max_delta_);
+    double br_cmd = clampDelta(last_br_out_, br_out_, max_delta_);
 
-    ros_phoenix::msg::MotorControl left_msg;
-    left_msg.mode  = ros_phoenix::msg::MotorControl::PERCENT_OUTPUT;
-    left_msg.value = left_cmd;
+    // ros_phoenix::msg::MotorControl left_msg;
+    // left_msg.mode  = ros_phoenix::msg::MotorControl::PERCENT_OUTPUT;
+    // left_msg.value = left_cmd;
 
-    ros_phoenix::msg::MotorControl right_msg;
-    right_msg.mode  = ros_phoenix::msg::MotorControl::PERCENT_OUTPUT;
-    right_msg.value = right_cmd;
+    // ros_phoenix::msg::MotorControl right_msg;
+    // right_msg.mode  = ros_phoenix::msg::MotorControl::PERCENT_OUTPUT;
+    // right_msg.value = right_cmd;
+
+    ros_phoenix::msg::MotorControl fl_msg;
+    ros_phoenix::msg::MotorControl fr_msg;
+    ros_phoenix::msg::MotorControl bl_msg;
+    ros_phoenix::msg::MotorControl br_msg;
+
+    fl_msg.mode = fr_msg.mode = bl_msg.mode = br_msg.mode = ros_phoenix::msg::MotorControl::PERCENT_OUTPUT;
+    fl_msg.value = fl_cmd;
+    fr_msg.value = fr_cmd;
+    bl_msg.value = bl_cmd;
+    br_msg.value = br_cmd;
 
     // Publish based on control mode: 0=rear, 1=front, 2=both
     if (control_mode_ == 1) {
-      pub_fl_->publish(left_msg);
-      pub_fr_->publish(right_msg);
+      pub_fl_->publish(fl_msg);
+      pub_fr_->publish(fr_msg);
     } else if (control_mode_ == 0) {
-      pub_bl_->publish(left_msg);
-      pub_br_->publish(right_msg);
+      pub_bl_->publish(bl_msg);
+      pub_br_->publish(br_msg);
     } else {
-      pub_fl_->publish(left_msg);
-      pub_bl_->publish(left_msg);
-      pub_fr_->publish(right_msg);
-      pub_br_->publish(right_msg);
+      pub_fl_->publish(fl_msg);
+      pub_bl_->publish(bl_msg);
+      pub_fr_->publish(fr_msg);
+      pub_br_->publish(br_msg);
     }
 
-    last_left_out_  = left_cmd;
-    last_right_out_ = right_cmd;
+    last_fl_out_ = fl_cmd;
+    last_fr_out_ = fr_cmd;
+    last_bl_out_ = bl_cmd;
+    last_br_out_ = br_cmd;
   }
 
   static double clampDelta(double last, double target, double max_delta) {
@@ -129,14 +156,16 @@ private:
 
   // State
   int control_mode_ = 2; // 0=rear, 1=front, 2=both
-  double left_out_ = 0.0, right_out_ = 0.0;
-  double last_left_out_ = 0.0, last_right_out_ = 0.0;
+  double fl_out_ = 0.0, fr_out_ = 0.0, bl_out_ = 0.0, br_out_ = 0.0;
+  double last_fl_out_ = 0.0, last_fr_out_ = 0.0, last_bl_out_ = 0.0, last_br_out_ = 0.0;
 
   // Params
-  double radius_{0.8};
+  double radius_inner_{0.8};
+  double radius_outer_{0.8};
   double max_linear_speed_{2.5};
-  double max_angular_speed_{radius_ * max_linear_speed_};
-  double max_delta_{0.02};
+  double max_inner_angular_speed_{radius_inner_ * max_linear_speed_};
+  double max_outer_angular_speed_{radius_outer_ * max_linear_speed_};
+  double max_delta_{0.015};
 };
 
 int main(int argc, char** argv) {
